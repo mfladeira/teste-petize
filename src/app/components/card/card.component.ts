0ff9b757985f1964de9d1bb7206af5d9fd5ac084
Mateus Ferreira <mateusferreira0704@gmail.com>
2024-03-07 14:59:30 -0300

    Modified card component

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,16 +1,21 @@
-import { NgIf, NgTemplateOutlet } from '@angular/common';
+import { NgIf } from '@angular/common';
 import { Component, Input } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
 
+interface Person {
+  id: number;
+  urlFoto: string;
+  nome: string;
+  idade: number;
+  sexo: string;
+}
 @Component({
   selector: 'app-card',
   standalone: true,
-  imports: [NgIf],
+  imports: [NgIf, RouterModule],
   templateUrl: './card.component.html',
   styleUrl: './card.component.scss'
 })
 export class CardComponent {
-  @Input() imgSrc = '';
-  @Input() name = '';
-  @Input() age = 0;
-  @Input() gender = '';
+  @Input() person!: Person;
 }
