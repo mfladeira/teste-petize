046079082ca5bc42e433641be48c4aba3ef39673
Mateus Ferreira <mateusferreira0704@gmail.com>
2024-03-07 15:00:23 -0300

    Added new route in api serach service

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { DetailsPersonComponent } from './pages/details-person/details-person.component';
 
 export const routes: Routes = [
     {
         path: '',
         component: HomeComponent
+    },
+    {
+        path: 'details/:id',
+        component: DetailsPersonComponent
     }
 ];
--- src/app/services/api-search-person.service.ts
+++ src/app/services/api-search-person.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
-import { catchError, retry, throwError } from 'rxjs';
+import { PersonDetails } from '../pages/details-person/details-person.component';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -10,7 +11,10 @@ export class ApiSearchPersonService {
   constructor(private http: HttpClient) { }
 
   getPeople(filter: string) {
-    return this.http.get('https://abitus-api.pjc.mt.gov.br/v1/pessoas/aberto/filtro?porPagina=12&status=DESAPARECIDO&pagina=0'.concat(filter));
+    return this.http.get('https://abitus-api.pjc.mt.gov.br/v1/pessoas/aberto/filtro?porPagina=12&status=DESAPARECIDO'.concat(filter));
   }
 
+  getPerson(id: string): Observable<PersonDetails> {
+    return this.http.get<PersonDetails>(`https://abitus-api.pjc.mt.gov.br/v1/pessoas/${id}`);
+  }
 }
